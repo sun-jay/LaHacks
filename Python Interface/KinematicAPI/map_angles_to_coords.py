ccfc73311269b06ec6ca2d01921b30369eeea86d
cozyrohan <rohan.jyrm@gmail.com>
2024-04-20 06:49:23 -0700

    mapping to dict

--- Python Interface/KinematicAPI/map_angles_to_coords.py
+++ Python Interface/KinematicAPI/map_angles_to_coords.py
@@ -6,12 +6,15 @@ import HTM
 def make_map(manip):
 
     map_dict = dict()
-    for x in range(0,181):
-        for y in range(0,181):
-            for z in range(0,181):
+    for a1 in range(0,181):
+        for a2 in range(0,181):
+            for a3 in range(0,181):
                 try:
                     manip.check_angles(x, y, z)
                     #if the angles are valid, continue
+
+                    map_dict[f'{a1} {a2} {a3}'] = HTM.get_xyz(a1, a2, a3) #angles in degreez
+
 
 
                 except AssertionError as e:
