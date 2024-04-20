bb63e5911befb1c3e1531c5e11031d895d6e3511
cozyrohan <rohan.jyrm@gmail.com>
2024-04-20 05:04:12 -0700

    Motor Serial comms done

--- Python Interface/KinematicAPI/serial_comm.py
+++ Python Interface/KinematicAPI/serial_comm.py
@@ -5,19 +5,20 @@ import os
 class Controller:
     def __init__(self):
         self.ser = None
-        self.default_position = [0,0,0]
-        self.cur = None
-        self.magnet = False
+        self.magnet = 0
+        self.current_position = [None, None, None]
+        self.default_position = "000 000 000 0"
+
 
         self.buadrate = 115200
-        self.connect()
 
-    def array_to_string(self, arr):
+
+    def curr_pos_to_string(self):
 
         # string must look like  "iii iii iii i". it must be 13 characters long, all angles will be 1-3 digits, so we need to pad with zeros
 
         # one liner
-        return " ".join([str(i).zfill(3) for i in arr])
+        return " ".join([str(i).zfill(3) for i in self.current_position]) + '0' + str(self.mag)
 
         
     def connect(self):
@@ -45,15 +46,40 @@ class Controller:
 
         self.ser = serial.Serial(f"/dev/{options[choice]}", self.buadrate)
         time.sleep(2)
-        print("Connected to serial")
+        print("Connected to serial.")
 
-    def send_signal(self, angles):
-        s = self.array_to_string(angles)
-        self.ser.write(s.encode())
+    def check_angles(self, a1, a2, a3):
+        '''
+        Here we will write conditions to not break the bot
+        '''
+        assert ( a1 in range(0, 181)), f"a1 not in servo range [0,180]. actual: {a1}"
+        assert ( a2 in range(0, 181)), f"a1 not in servo range [0,180]. actual: {a2}"
+        assert ( a2 in range(0, 181)), f"a1 not in servo range [0,180]. actual: {a2}"
+
+        assert ( a1 + a2 < 1801)
         
 
+    def send_signal(self, a1, a2, a3, mag):
 
-        
+        #check the angles
+        try:
+
+            self.check_angles(a1, a2, a3)
+            #if this raises no issues, continue
+
+            #set the curr_poss and mag status
+            self.current_position = [a1, a2, a3]
+            self.mag = mag
+
+            #turn it into a string
+            angle_str = self.curr_pos_to_string()
+            
+            #send said string
+            self.ser.write(angle_str.encode())
+
+        except AssertionError as e:
+            print("Sending Angles Failed")
+            print(e)
 
     def close_connection(self):
         self.ser.close()
@@ -64,13 +90,15 @@ class Controller:
 
         
 if __name__ == "__main__":
-    c = Controller()
     try: 
-        c.send_signal([90,90,90])
-        time.sleep(1)
-        c.send_signal([0,0,0])
-        time.sleep(1)
-        c.send_signal([90,90,90])
+        manip = Controller()
+        manip.connect()
+
+        while(True):
+            a1,a2,a3,M = [int(i) for i in input("Enter 3 angles and mag: 'a1 a2 a3 M': ").strip().split()]
+            manip.send_signal(a1,a2,a3,M)
+    except AssertionError as e:
+        print(e)
     finally:
-        c.close_connection()
+        manip.close_connection()
     
